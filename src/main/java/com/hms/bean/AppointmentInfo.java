9c78b092c56ee8db137c693cf8bcee86e85859b8
Bijay <kbijaykumar19@gmail.com>
2021-04-14 11:19:02 +0530

    initial commit

---
+++ src/main/java/com/hms/bean/AppointmentInfo.java
@@ -0,0 +1,60 @@
+package com.hms.bean;
+
+public class AppointmentInfo {
+
+	private String name;
+	private String mob;
+	private String address;
+	private String appointmentDate;
+	private String previousVisitDate;
+	private String previousAppointmentSerialNo;
+	private String registrationNo;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMob() {
+		return mob;
+	}
+	public void setMob(String mob) {
+		this.mob = mob;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAppointmentDate() {
+		return appointmentDate;
+	}
+	public void setAppointmentDate(String appointmentDate) {
+		this.appointmentDate = appointmentDate;
+	}
+	public String getPreviousVisitDate() {
+		return previousVisitDate;
+	}
+	public void setPreviousVisitDate(String previousVisitDate) {
+		this.previousVisitDate = previousVisitDate;
+	}
+	public String getPreviousAppointmentSerialNo() {
+		return previousAppointmentSerialNo;
+	}
+	public void setPreviousAppointmentSerialNo(String previousAppointmentSerialNo) {
+		this.previousAppointmentSerialNo = previousAppointmentSerialNo;
+	}
+	public String getRegistrationNo() {
+		return registrationNo;
+	}
+	public void setRegistrationNo(String registrationNo) {
+		this.registrationNo = registrationNo;
+	}
+	
+	
+	
+	
+	
+	
+}
